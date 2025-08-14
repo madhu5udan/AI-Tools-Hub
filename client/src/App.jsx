20297eb0e0d447728a21e8d12ac781e373924467
Madhusudan <madhusudan63633@gmail.com>
2025-08-14 18:48:30 +0530

    Home and Menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import Layout from './pages/Layout'
-import WriteArticle from './pages/WriteArticle'
-import BlogTitles from './pages/BlogTitles'
-import Dashboard from './pages/Dashboard'
-import RemoveBackground from './pages/RemoveBackground'
-import RemoveObject from './pages/RemoveObject'
-import ReviewResume from './pages/ReviewResume'
-import Community from './pages/Community'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitles from "./pages/BlogTitles";
+import Dashboard from "./pages/Dashboard";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
+import GenerateImages from "./pages/GenerateImages";
 
 function App() {
   return (
-    <div >
+    <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/ai' element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path='write-article' element={<WriteArticle />} />
-          <Route path='blog-titles' element={<BlogTitles />} />
-          <Route path='remove-background' element={<RemoveBackground />} />
-          <Route path='remove-object' element={<RemoveObject />} />
-          <Route path='review -resume' element={<ReviewResume />} />
-          <Route path='community' element={<Community />} />
-
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
